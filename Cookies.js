d8a829be408f2900d5ecea1c641dc01474526cc2
ajmat6 <114720514+ajmat6@users.noreply.github.com>
2023-05-15 11:46:21 +0530

    Cookies in JS

---
+++ Cookies.js
@@ -0,0 +1,13 @@
+console.log(document.cookie) // will show all cookies
+// document.cookie = "name=ajmat"; // adding a cookie
+// console.log(document.cookie);
+// document.cookie = "name2=kathat"; // adding a cookie
+// console.log(document.cookie); // will show both added cookies (it will not overwrite)
+// document.cookie = "name=aslam"; // adding a cookie
+// console.log(document.cookie); // will update name as aslam(it will not overwrite)
+
+let key = prompt("Enter your cookie key");
+let value = prompt("Enter your cookie value");
+
+document.cookie = `${encodeURIComponent(key)}=${encodeURIComponent(value)}`;
+console.log(document.cookie); 
